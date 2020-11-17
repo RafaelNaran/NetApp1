979abc6a4906d8f59c12885da64bebee8cbdbc8e
alex15v <16040104@itsuruapan.edu.mx>
2020-11-17 17:32:45 -0600

    El cambio del Broo

--- app/src/main/java/com/example/netapp/Menu.java
+++ app/src/main/java/com/example/netapp/Menu.java
@@ -11,7 +11,7 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        //Probando Cambios
+        //Probando Cambios Hey Broo
     }
 
 
